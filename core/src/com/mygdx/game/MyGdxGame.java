02fc808ce6c23edad7b80b4a370c36914c238bcd
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-05-21 18:47:36 +0400

    Бегает по щелчку мышки, клавиши не доделала

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,36 +1,39 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screen.ScreenMenu;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	Texture backtexture;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		backtexture  = new Texture("kosmos.jpg");
+		setScreen(new ScreenMenu());
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+//		backtexture  = new Texture("kosmos.jpg");
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(backtexture , 0, 0);
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		backtexture.dispose();
-	}
+//	@Override
+//	public void render () {
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(backtexture , 0, 0);
+//		batch.draw(img, 0, 0);
+//		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//		backtexture.dispose();
+//	}
 }
