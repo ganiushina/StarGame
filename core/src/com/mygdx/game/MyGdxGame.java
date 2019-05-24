f6c780b0303f2cec0cfb93bdbb2782fa92b236ba
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-05-24 19:03:38 +0400

    Бежит в нужную сторону, но в неправильную точку. По координатам, соотвественно, не останавливается. А если считать длину вектора, но считает неправильно и останавливается не в том месте.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,31 +9,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Screen.ScreenMenu;
 
 public class MyGdxGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	Texture backtexture;
-
 	@Override
 	public void create () {
 		setScreen(new ScreenMenu());
-//		batch = new SpriteBatch();
-//		img = new Texture("badlogic.jpg");
-//		backtexture  = new Texture("kosmos.jpg");
 	}
-
-//	@Override
-//	public void render () {
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		batch.draw(backtexture , 0, 0);
-//		batch.draw(img, 0, 0);
-//		batch.end();
-//	}
-//
-//	@Override
-//	public void dispose () {
-//		batch.dispose();
-//		img.dispose();
-//		backtexture.dispose();
-//	}
 }
